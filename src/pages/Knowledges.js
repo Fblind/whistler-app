52077ec069a4f4c2584b72afe1df1dde3f6905a4
Fblind <fblind@gmail.com>
2021-09-02 16:43:04 -0300

    filter kb by tags

--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -4,12 +4,14 @@ import './Knowledges.css'
 
 function Knowledges () {
   const [knowledges, setKnowledges] = useState([])
+  const [allKnowledges, setAllKnowledges] = useState([])
   const [topics, setTopics] = useState([])
   useEffect(() => {
     fetch('/knowledges')
       .then(r => r.json())
       .then(knowledges => {
         setKnowledges(knowledges)
+        setAllKnowledges(knowledges)
         const knowledgesTopics = knowledges
           .map(k => k.tags)
           .reduce((accum, t) => accum.concat(t), [])
@@ -18,14 +20,23 @@ function Knowledges () {
       })
   }, [])
 
+  const handleTopicClick = (topic) => {
+    const newKnowledges = allKnowledges.filter(kb => kb.tags.includes(topic));
+    setKnowledges(newKnowledges);
+  }
+
+  const resetKnowledges = () => {
+    setKnowledges(allKnowledges)
+  }
+
   return (
     <div className="mt-6 w-full px-4">
       <h1 className="text-2xl font-bold text-gray-900 mb-6">Directory</h1>
       <div className="flex flex-row justify-between items-center">
         <h2 className="text-lg text-gray-600 font-bold">Topics</h2>
-        <a className="text-indigo-600 text-sm">View all</a>
+        <a className="text-indigo-600 text-sm cursor-pointer" onClick={resetKnowledges}>View all</a>
       </div>
-      <TopicCarousel topics={topics}></TopicCarousel>
+      <TopicCarousel topics={topics} onTopicClick={handleTopicClick}></TopicCarousel>
       <div className="flex flex-row justify-between items-center">
         <h2 className="text-lg text-gray-600 font-bold">Recents</h2>
         <a className="text-indigo-600 text-sm">View all</a>
@@ -49,7 +60,7 @@ function Carousel ({children}) {
   )
 }
 
-function TopicCarousel ({ topics }) {
+function TopicCarousel ({ topics, onTopicClick }) {
   const getRandom = (list) => {
     const index = Math.floor(Math.random() * list.length)
     return list[index]
@@ -58,7 +69,7 @@ function TopicCarousel ({ topics }) {
     const colors = ['red', 'blue', 'pink', 'yellow', 'green']
     const color = getRandom(colors)
     return (
-      <div key={topic} className="shadow-lg rounded-lg mr-2 inline-block">
+      <div key={topic} className="shadow-lg rounded-lg mr-2 inline-block cursor-pointer" onClick={() => onTopicClick(topic)}>
         <div className="m-w-24 h-24 p-2 flex flex-col justify-between">
           <div className={`w-3 h-3 bg-${color}-600 rounded-full`}></div>
           <p className="text-gray-900 font-bold">{topic}</p>
